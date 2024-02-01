b45e356c2a9ce6b911baf3be943106eb268372b3
boogie1989 <sergeybugai@gmail.com>
2024-02-01 21:19:08 +0200

    homework for theme 3

--- theme_2/task_1.py
+++ theme_2/task_1.py
@@ -5,11 +5,13 @@ import random
 # Створення черги заявок
 requests_queue = Queue()
 
+
 def generate_request():
     """Функція для генерації нової заявки."""
     request_id = random.randint(1, 1000)  # Генеруємо унікальний ID для заявки
     print(f"Генеруємо нову заявку з ID: {request_id}")
     requests_queue.put(request_id)  # Додаємо заявку до черги
+
 
 def process_request():
     """Функція для обробки заявки з черги."""
@@ -18,6 +20,7 @@ def process_request():
         print(f"Обробляємо заявку з ID: {request_id}")
     else:
         print("Черга пуста")
+
 
 def main_loop():
     """Головний цикл програми для імітації роботи сервісного центру."""
@@ -28,5 +31,6 @@ def main_loop():
             time.sleep(1)  # Чекаємо 1 секунду перед наступною ітерацією
     except KeyboardInterrupt:
         print("Програма завершена користувачем")
+
 
 main_loop()
--- theme_2/task_2.py
+++ theme_2/task_2.py
@@ -1,18 +1,21 @@
 from collections import deque
 
+
 def is_palindrome(s):
     # Підготовка рядка: видалення пробілів та перетворення на нижній регістр
     cleaned_str = ''.join(s.split()).lower()
-    
+
     # Створення двосторонньої черги з символів рядка
     char_deque = deque(cleaned_str)
-    
+
     while len(char_deque) > 1:
         if char_deque.popleft() != char_deque.pop():
             return False
     return True
 
+
 # Тестування функції
-test_strings = ["A man a plan a canal Panama", "Madam In Eden I'm Adam", "Not a palindrome"]
+test_strings = ["A man a plan a canal Panama",
+                "Madam In Eden I'm Adam", "Not a palindrome"]
 for test_str in test_strings:
     print(f"\"{test_str}\" - це паліндром? {is_palindrome(test_str)}")
